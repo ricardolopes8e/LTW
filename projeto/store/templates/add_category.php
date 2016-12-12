c1e6db6a74a9aa4334de79d2e7101334504884b7
nice5torm <marianadguimaraes@hotmail.com>
2016-12-12 22:19:35 +0000

    remove files

--- projeto/store/templates/add_category.php
+++
@@ -1,9 +0,0 @@
-<section id="content">
-  <h2>Add Category</h2>
-  <form action="action_add_category.php" method="post">
-    <label>Name:
-      <input type="text" name="name">
-    </label>
-    <input type="submit">
-  </form>
-</section>
