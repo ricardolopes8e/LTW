59e9892e9d2cb4617c597cecd49fa2513bf44426
ricardolopes8e <ricardolopes8e@gmail.com>
2016-12-15 14:00:54 +0000

    corrreçao

--- projeto/store/list_of_restaurants.php
+++ projeto/store/list_of_restaurants.php
@@ -7,7 +7,7 @@
 
   $id_rest =getRestaurant_ID($name);
   $image=getRestaurant_image($id_rest);
-  include ('templates/header.php');
+  include ('templates/header_review.php');
   include ('templates/navigation.php');
   include ('templates/restaurant_header.php');
   include ('templates/result_search_restaurant.php');
