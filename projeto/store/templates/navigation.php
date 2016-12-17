017a2c01316e601987a9a81357b734f818c8e1ce
ricardolopes8e <ricardolopes8e@gmail.com>
2016-12-17 12:35:12 +0000

    css e loginpage

--- projeto/store/templates/navigation.php
+++ projeto/store/templates/navigation.php
@@ -1,17 +1,20 @@
 <nav>
-  <ul>  
+  <ul>
 	<?php
        if (isset($_SESSION['username'])) {
          include ('templates/logout_form.php');?>
 	   <li><a href="edit_user.php">Edit User</a></li>
 	   <li><a href="see_user.php">User</a></li>
 	<?php   }
-       else    
-         include ('templates/login_form.php');
+       else
+         //include ('templates/login_form.php');
 	?>
+    <div id="Buttons">
        <li><a href="list_restaurants.php">Restaurants</a></li>
     <?php if (!isset($_SESSION['username'])) { ?>
          <li><a href="register.php">Register</a></li>
+         <li><a href="logInPage.php">Log in</a></li>
+      </div>
     <?php } ?>
   </ul>
 </nav>
