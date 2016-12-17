22502d261fdc2d95d861fc911fdc44fe4f619302
Luis-bcruz <luis.labc95@gmail.com>
2016-12-17 16:51:53 +0000

    changed data base

--- projeto/store/food.sql
+++ projeto/store/food.sql
@@ -1,23 +1,30 @@
-CREATE TABLE owner (
-	username VARCHAR(255) NOT NULL REFERENCES user(username) ,
-	id_restaurant INTEGER NOT NULL REFERENCES restaurant(id) ,
-	PRIMARY KEY(username, id_restaurant)
-);
+CREATE TABLE Restaurant(
+			idRestaurant INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR NOT NULL,
+			owner VARCHAR REFERENCES User(username),
+			timetable VARCHAR,
+			contact INTEGER,
+			address VARCHAR,
+			idCity INTEGER REFERENCES City(idCity)
+			);		
 
-CREATE TABLE restaurant (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,	
-	name VARCHAR(255) NOT NULL,
-	user_owner VARCHAR(255) NOT NULL REFERENCES owner(username) ,
-	timetable VARCHAR,
-	contact INTEGER,
-	adress VARCHAR(255),
-	description VARCHAR(255) NOT NULL,
-	idCity INTEGER REFERENCES City(idCity)
- );
- 
-INSERT INTO restaurant VALUES(1,'BB Vila', 'LuisCruz','Segunda - Domingo 12:00h - 3:00h',229383374,'Rua das Larangas','Um restaurante da cadeira BB',1);
-INSERT INTO restaurant VALUES(2,'BB Gourmet', 'MiguelCruz', 'Segunda-Sabado 12:00h - 2:00h',221122112,'Rua dos morangos', 'Um restaurante Gourmet',2);
-INSERT INTO restaurant VALUES(3,'McDonals Foz', 'LuisCruz','Segunda-Sexta 7:00h - 4:00h',220101010,'Avenida Brasil', 'Um restaurante na Foz',2);
+INSERT INTO Restaurant VALUES(1,'BB Vila', 'LuisCruz','Segunda - Domingo 12:00h - 3:00h',229383374,'Rua das Larangas', 1);
+INSERT INTO Restaurant VALUES(2,'BB Gourmet', 'MiguelCruz', 'Segunda-Sabado 12:00h - 2:00h',221122112,'Rua dos morangos', 2);
+INSERT INTO Restaurant VALUES(3,'McDonals Foz', 'LuisCruz','Segunda-Sexta 7:00h - 4:00h',220101010,'Avenida Brasil', 2);
+
+CREATE TABLE Review(
+			idreview INTEGER PRIMARY KEY AUTOINCREMENT,
+			idRestaurant INTEGER,
+			username VARCHAR,
+			rating INTEGER,
+			dateAval DATE,
+			comment VARCHAR,
+			FOREIGN KEY(idRestaurant) REFERENCES Restaurant(idRestaurant),
+			FOREIGN KEY(username) REFERENCES User(username)
+			);
+
+INSERT INTO Review VALUES(1, 1, 'LuisCruz',5,'22-03-2016','Melhor local para ir jantar e receber um bolo de aniversário!');
+INSERT INTO Review VALUES(2, 1, 'MiguelCruz',4,'22-03-2016','Boa refeição a preços acessiveis. Oferta de bolo em caso de aniversário.');
 
 CREATE TABLE City(
 			idCity INTEGER PRIMARY KEY,
@@ -28,53 +35,44 @@ INSERT INTO City VALUES(1,'Porto - Francos');
 INSERT INTO City VALUES(2,'Porto - Foz');
 
 
- CREATE TABLE review (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	id_restaurant INTEGER REFERENCES restaurant(id),
-	username VARCHAR(255) NOT NULL REFERENCES user(username),	
-	dateAval DATE,
-	rating INTEGER,
-	comment VARCHAR(255) NOT NULL
-  );
-  
-INSERT INTO review VALUES(1, 2, 'LuisCruz','22-03-2016',5,'Melhor local para ir jantar e receber um bolo de aniversário!');
-INSERT INTO review VALUES(2, 2, 'MiguelCruz','22-03-2016',4,'Boa refeição a preços acessiveis. Oferta de bolo em caso de aniversário.');
+CREATE TABLE Foto(
+			idFoto INTEGER PRIMARY KEY , 
+			fotoName VARCHAR
+			);
 
- CREATE TABLE user (
-  username VARCHAR(255),
-  password VARCHAR(255),
-  email VARCHAR(255),
-  description VARCHAR(255), 
-  PRIMARY KEY(username)
- );
- 
-INSERT INTO user VALUES('LuisCruz','$2y$12$ka2aGPQmODWNu81L1Ibdsu3zvCEry.WBJd2f2OlNaklFkkB3Vn3pO','luis.labc95@gmail.com',NULL);
-INSERT INTO user VALUES('MiguelCruz','$2y$12$ka2aGPQmODWNu81L1Ibdsu3zvCEry.WBJd2f2OlNaklFkkB3Vn3pO','miguel@fe.up.pt', NULL);
+		
+INSERT INTO Foto VALUES(1, 'img1.jpg');
+INSERT INTO Foto VALUES(2, 'img2.jpg');
+INSERT INTO Foto VALUES(3, 'img3.jpg');
+INSERT INTO Foto VALUES(4, 'img4.jpg');
+INSERT INTO Foto VALUES(5, 'img5.jpg');
+INSERT INTO Foto VALUES(6, 'img6.jpg');
+INSERT INTO Foto VALUES(7, 'img7.jpg');
+INSERT INTO Foto VALUES(8, 'img8.jpg');
+INSERT INTO Foto VALUES(9, 'img9.jpg');
+INSERT INTO Foto VALUES(10,'img10.jpg');
+INSERT INTO Foto VALUES(11,'img11.jpg');
+
+CREATE TABLE User(
+			username VARCHAR PRIMARY KEY,
+			password VARCHAR,
+			email VARCHAR,
+			idCity REFERENCES City(idCity)
+			);
+
+INSERT INTO User VALUES('LuisCruz','$2y$12$ka2aGPQmODWNu81L1Ibdsu3zvCEry.WBJd2f2OlNaklFkkB3Vn3pO','luis.labc95@gmail.com', 2);
+INSERT INTO User VALUES('MiguelCruz','$2y$12$ka2aGPQmODWNu81L1Ibdsu3zvCEry.WBJd2f2OlNaklFkkB3Vn3pO','miguel@fe.up.pt', 2);
 			
-CREATE TABLE photo(
-			id_photo INTEGER PRIMARY KEY , 
-			photo_name VARCHAR
-			);
-
-INSERT INTO photo VALUES(1, 'img1.jpg');
-INSERT INTO photo VALUES(2, 'img2.jpg');
-INSERT INTO photo VALUES(3, 'img3.jpg');
-INSERT INTO photo VALUES(4, 'img4.jpg');
-INSERT INTO photo VALUES(5, 'img5.jpg');
-INSERT INTO photo VALUES(6, 'img6.jpg');
-INSERT INTO photo VALUES(7, 'img7.jpg');
-INSERT INTO photo VALUES(8, 'img8.jpg');
-INSERT INTO photo VALUES(9, 'img9.jpg');
-INSERT INTO photo VALUES(10,'img10.jpg');
-INSERT INTO photo VALUES(11,'img11.jpg');
-
-
-CREATE TABLE user_photo(
-			id_photo INTEGER,
+CREATE TABLE UserFoto(
+			idFoto INTEGER,
 			username VARCHAR, 
-			FOREIGN KEY(id_photo) REFERENCES photo(id_photo),
-			FOREIGN KEY(username) REFERENCES user(username),
-			PRIMARY KEY (id_photo)
+			FOREIGN KEY(idFoto) REFERENCES Foto(idFoto),
+			FOREIGN KEY(username) REFERENCES User(username),
+			PRIMARY KEY (idFoto)
 			);
 
-INSERT INTO user_photo VALUES(11, 'LuisCruz');
+INSERT INTO UserFoto VALUES(11, 'LuisCruz');
+
+
+
+
