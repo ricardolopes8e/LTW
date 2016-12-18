da3f57bc7c37bd8764fb4179c842683fff3d1aac
Luis-bcruz <luis.labc95@gmail.com>
2016-12-18 20:35:43 +0000

    added css to View Restaurant detail info page

--- projeto/store/RestaurantDetailPage.php
+++ projeto/store/RestaurantDetailPage.php
@@ -1,70 +1,70 @@
 <?php
-	include_once('config/init.php');
-	include_once("getRestaurantInfo.php");
-	include_once('database/photo.php'); 
+include_once('config/init.php');
+include_once('getRestaurantInfo.php');
+include_once('database/photo.php'); 
 
-//	include('templates/headerFinal.php');
-	
-	$Restaurant_search = $_GET['Restaurant_search'];
+include('templates/headerFinal.php');
+include('templates/Corner_food.php');
 
-	$restaurante = getRestaurant_name($Restaurant_search);
-	$res = getInfo_restaurant($Restaurant_search); 
 
-	$info_restaurant_photo = getInfo_restaurant_photo($Restaurant_search);
-	$name = $res['name']; 
-	$owner = $res['owner']; 
-	$timetable = $res['timetable']; 
-	$contact = $res['contact']; 
-	$address = $res['address'];
-	
-	$idFoto = $info_restaurant_photo['idFoto'];
-	$infoFoto = getInfoFoto($idFoto);
-	$FotoName = $infoFoto['fotoName'];
+$Restaurant_search = $_GET['Restaurant_search'];
 
-    $Rating = getAverage($Restaurant_search);
-   
-	$stmt = $conn->prepare('SELECT * FROM Review WHERE Review.idRestaurant = ?');
-    $stmt->execute(array($Restaurant_search));
-    $resultreview = $stmt->fetchAll();
-	  
-	?>
-	<section id="details restaurant">
-	<h2>Restaurant Detail</h2>
+$restaurante = getRestaurant_name($Restaurant_search);
+$res = getInfo_restaurant($Restaurant_search); 
+
+$info_restaurant_photo = getInfo_restaurant_photo($Restaurant_search);
+$name = $res['name']; 
+$owner = $res['owner']; 
+$timetable = $res['timetable']; 
+$contact = $res['contact']; 
+$address = $res['address'];
+
+$idFoto = $info_restaurant_photo['idFoto'];
+$infoFoto = getInfoFoto($idFoto);
+$FotoName = $infoFoto['fotoName'];
+
+$Rating = getAverage($Restaurant_search);
+
+$stmt = $conn->prepare('SELECT * FROM Review WHERE Review.idRestaurant = ?');
+$stmt->execute(array($Restaurant_search));
+$resultreview = $stmt->fetchAll();
+
+?>
+<section id="Restaurant-Detailed-Info">
+	<!-- <h2>Restaurant Detail</h2> -->
 	<header>
-	
-    </section>
-		<?php if($FotoName !== NULL){?>
-		<section>
-		<img src="database/photos/<?=$FotoName?>" width="200" height="200">
+		<section id="Restaurant-Info">
+			<?php if($FotoName !== NULL)
+			{?>
+			<section id="Restaurant-Image">
+				<img src="database/photos/<?=$FotoName?>" width="200" height="200">
+			</section>
+			<?php } ?>
+			<section id="name">
+				Name: <h3><?=$name?></h3>
+			</section>
+			<section id="owner">
+				Owner: <h3><?=$owner?></h3>
+			</section>	
+			<section id="timetable">
+				Timetable: <h3><?=$timetable?></h3>
+			</section>
+			<section id="contact">
+				Contact: <h3><?=$contact?></h3>
+			</section>
+			<section id="address">
+				Address: <h3><?=$address?></h3>
+			</section>
+			<section id="rating">
+				Rating: <h3><?=$Rating?> stars</h3>
+			</section>
 		</section>
-		<?php } ?>
-		<section>
-			Name: <h3 id="name"><?=$name?></h3>
-		</section>
-		<section>
-			Owner: <h3 id="owner"><?=$owner?></h3>
-		</section>	
-		<section>
-			Timetable: <h3 id="timetable"><?=$timetable?></h3>
-		</section>
-		<section>
-			Contact: <h3 id="contact"><?=$contact?></h3>
-		</section>
-		<section>
-			Address: <h3 id="address"><?=$address?></h3>
-		</section>
-		<section>
-			Rating: <h3 id="rating"><?=$Rating?></h3>
-		</section>
-		<a href="RestaurantDetailPage.php?Restaurant_search=<?=$resultreview?>#info"> Ver detalhes do restaurante </a>
-		
 	</header>
-	
-	 <li><a href="index.php">Main Page</a></li>
+
+	<li><a href="index.php">Main Page</a></li>
 </section>
 
 <?php
 
-//include('templates/detalhesRestaurante.php');
-	include('templates/footer.php');
+include('templates/footer.php');
 ?>
