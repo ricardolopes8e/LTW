7e50bff104dcee27d8e61034a87d950f3800842b
Luis-bcruz <luis.labc95@gmail.com>
2016-12-18 18:03:54 +0000

    changed  index path

--- projeto/store/index.php
+++ projeto/store/index.php
@@ -1,6 +1,7 @@
 <?php
-  //header ('Location: mainpage.php');
-	include_once('config/init.php');
-	include('templates/headerFinal.php');
-	include('templates/footer.php');
+include_once('config/init.php');
+include('templates/headerFinal.php');
+include('templates/Corner_food.php');
+include('templates/search_restaurant_form.php');
+include('templates/footer.php');
 ?>
