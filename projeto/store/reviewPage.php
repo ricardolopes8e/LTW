915af72fd9cf6693e211f323ff1e344531881a82
ricardolopes8e <ricardolopes8e@gmail.com>
2016-12-18 13:31:15 +0000

    list of reviews updated

---
+++ projeto/store/reviewPage.php
@@ -0,0 +1,7 @@
+<?php
+  include_once('config/init.php');
+
+  include ('templates/header_review.php');
+  //include('templates/search_specific_restaurant');
+  include('templates/result_search_reviews.php');
+?>
---
+++ projeto/store/templates/result_search_reviews.php
@@ -0,0 +1,61 @@
+<?php
+  include_once('config/init.php');
+
+   global $conn;
+
+   $stmt = $conn->prepare('SELECT * FROM Review WHERE idRestaurant = ?');
+     $stmt->execute(array($_GET["idRestaurant"]));
+     $reviews = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  ?>
+<section id="review_container">
+  <ul class="reviews">
+
+    <?php
+
+    $i=0;
+    foreach ($reviews as $review) {
+      foreach ($review as $review_element){
+      $i=$i+1;
+
+	?>
+  <?php
+  switch ($i){
+    case "1":break;
+    case "2":break;
+    case "3"://echo "username:";
+    $username=$review_element;
+    //echo "<br>";
+      break;
+    case "4"://echo "rating:";
+    $rating=$review_element;
+    //echo "<br>";
+    break;
+    case "5"://echo "date:";
+     $date=$review_element;
+    //echo "<br>";
+    break;
+    case "6"://echo "comment:";
+     $comment=$review_element;
+    //echo "<br>";
+    $i=0;
+    break;
+  }
+  //echo "<br>";
+}
+?>
+  <div class="news-item">
+    <div><p class="introduction"><?=$comment?></p></div>
+    <div class="clear"> </div>
+  </div>
+  <div id="info">
+  <ul>
+  <li>name:<?=$username?></li>
+  <li>Date:<?=$date?></li>
+  <li>Rating:<?=$rating?></li>
+  </ul>
+  </div>
+
+
+    <?php } ?>
+  </ul>
+</section>
