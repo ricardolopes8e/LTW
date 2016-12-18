2f96e2c8acce1eb7a46c6ca896692c98de1f5f9b
Luis-bcruz <luis.labc95@gmail.com>
2016-12-18 18:39:06 +0000

    updated edit user page

--- projeto/store/edit_user.php
+++ projeto/store/edit_user.php
@@ -1,8 +1,9 @@
 <?php
   include_once('config/init.php');
   
+  include('templates/headerFinal.php');
   include ('templates/headerEdit.php');
- // include ('templates/video_header.php');
+
   include ('templates/edit_user_template.php');
   include ('templates/footer.php');
 ?>
